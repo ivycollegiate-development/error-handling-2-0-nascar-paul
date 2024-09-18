d36f903b5dacca5aaf642dd7ee17796308c8f297
Paul Jones <paul.tyler.jones@gmail.com>
2024-09-18 05:41:51 +0000

    Comments out invalid code

--- Sprint_Project_1.3.py
+++ Sprint_Project_1.3.py
@@ -1,6 +1,10 @@
 try:
-    age=int(input("Please enter your age: "))
+    # This is the code that may generate a failure in the program
+    age=int(input("Please enter your age: ")) # This is also considered a comment
+    """
+    Anything in this space will not be EXECUTED
+    """
 except ValueError:
     print("This is not a valid response")
-
-print("In one year, you will be ", age + 1)
+# There is no point in telling someone why something has already failed
+# print("In one year, you will be ", age + 1)
