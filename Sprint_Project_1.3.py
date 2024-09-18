3cb367640b452606df1b8a42c79b175bd6cc95ef
Paul Jones <paul.tyler.jones@gmail.com>
2024-09-18 05:51:29 +0000

    Comments line 4

--- Sprint_Project_1.3.py
+++ Sprint_Project_1.3.py
@@ -1,6 +1,7 @@
 try:
     # This is the code that may generate a failure in the program
     age=int(input("Please enter your age: ")) # This is also considered a comment
+    # Below is another way to comment out code
     """
     Anything in this space will not be EXECUTED
     """
